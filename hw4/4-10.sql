71eb0440f6f49ea1ad9d80bf34886e672e63e76a
orange2120 <orange21201@gmail.com>
2019-05-28 16:55:04 +0800

    0528 hw4 report updated

--- hw4/4-10.sql
+++ hw4/4-10.sql
@@ -7,6 +7,6 @@ SELECT  DISTINCT count(J.JobTitle) FROM JOB AS J
 JOIN ASSIGNMENT AS A ON J.JobId = A.JobId
 GROUP BY J.JobTitle
 ORDER BY count(J.JobTitle) DESC
-LIMIT 2,0) -- 2,0 represents 3rd largest number
+LIMIT 2,1) -- 2,1 represents 3rd largest number
 
 ORDER BY count(J.JobTitle) DESC
